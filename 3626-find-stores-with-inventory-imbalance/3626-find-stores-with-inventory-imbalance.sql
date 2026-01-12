c5ad17bbe23720fef0d82f5a40f42d45928cc4c6
Pranay Shukla <pranayshukla1020@gmail.com>
2026-01-12 22:21:42 +0530

    Time: 617 ms (12.65%), Space: 0B (100%) - LeetHub

--- 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
+++ 3626-find-stores-with-inventory-imbalance/3626-find-stores-with-inventory-imbalance.sql
@@ -1,29 +1,30 @@
-WITH ranked_products AS (
+WITH rank_product AS (
     SELECT
         s.store_id,
         s.store_name,
         s.location,
+        i.inventory_id,
         i.product_name,
         i.quantity,
         i.price,
-        ROW_NUMBER() OVER (PARTITION BY s.store_id ORDER BY i.price DESC) AS price_desc_rn,
-        ROW_NUMBER() OVER (PARTITION BY s.store_id ORDER BY i.price ASC)  AS price_asc_rn,
+        ROW_NUMBER() OVER (PARTITION BY s.store_id ORDER BY i.price DESC) AS high_rank,
+        ROW_NUMBER() OVER (PARTITION BY s.store_id ORDER BY i.price ASC)  AS low_rank,
         COUNT(*) OVER (PARTITION BY s.store_id) AS product_count
     FROM stores s
     JOIN inventory i
         ON s.store_id = i.store_id
 ),
 
-store_extremes AS (
+conditions AS (
     SELECT
         store_id,
         store_name,
         location,
-        MAX(CASE WHEN price_desc_rn = 1 THEN product_name END) AS most_exp_product,
-        MAX(CASE WHEN price_desc_rn = 1 THEN quantity END)     AS most_exp_quantity,
-        MAX(CASE WHEN price_asc_rn  = 1 THEN product_name END) AS cheapest_product,
-        MAX(CASE WHEN price_asc_rn  = 1 THEN quantity END)     AS cheapest_quantity
-    FROM ranked_products
+        MAX(CASE WHEN high_rank = 1 THEN product_name END) AS most_exp_product,
+        MAX(CASE WHEN high_rank = 1 THEN quantity END)     AS most_exp_quantity,
+        MAX(CASE WHEN low_rank  = 1 THEN product_name END) AS cheapest_product,
+        MAX(CASE WHEN low_rank  = 1 THEN quantity END)     AS cheapest_quantity
+    FROM rank_product
     WHERE product_count >= 3
     GROUP BY store_id, store_name, location
 )
@@ -35,6 +36,7 @@ SELECT
     most_exp_product,
     cheapest_product,
     ROUND(cheapest_quantity / most_exp_quantity, 2) AS imbalance_ratio
-FROM store_extremes
+FROM conditions
 WHERE most_exp_quantity < cheapest_quantity
 ORDER BY imbalance_ratio DESC, store_name ASC;
+
