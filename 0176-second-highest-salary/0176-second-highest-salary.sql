498a455e1303733d1d9aa0d90dc8537a677d30b0
Pranay Shukla <pranayshukla1020@gmail.com>
2026-01-14 05:31:05 +0530

    Time: 281 ms (25.59%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,10 +1,9 @@
-WITH ranked_salaries AS (
-    SELECT
-        salary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+WITH second_highest AS (
+    SELECT id,
+           salary,
+           DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
     FROM Employee
 )
-SELECT
-    MAX(salary) AS SecondHighestSalary
-FROM ranked_salaries
-WHERE rnk = 2;
+SELECT MAX(salary) AS SecondHighestSalary
+FROM second_highest
+WHERE rnk = 2
