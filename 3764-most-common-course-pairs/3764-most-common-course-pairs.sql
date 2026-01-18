9129fe358bea9e66792f790a72f39898e324ad29
Pranay Shukla <pranayshukla1020@gmail.com>
2026-01-18 21:12:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3764-most-common-course-pairs/3764-most-common-course-pairs.sql
+++ 3764-most-common-course-pairs/3764-most-common-course-pairs.sql
@@ -1,26 +1,25 @@
 # Write your MySQL query statement below
-WITH filter_data AS (
+WITH top_performer AS (
     SELECT user_id
     FROM course_completions
     GROUP BY user_id
     HAVING AVG(course_rating) >= 4 AND COUNT(*) >= 5
 ),
-sequence_courses AS (
-    SELECT b.course_name AS first_course,
-           a.user_id,
-           LEAD(b.course_name) OVER (PARTITION BY user_id ORDER BY completion_date) AS second_course
-    FROM filter_data a 
+course_sequence AS (
+    SELECT a.user_id,
+           b.course_name AS first_course,
+           LEAD(b.course_name) OVER (PARTITION BY a.user_id ORDER BY completion_date) AS second_course
+    FROM top_performer a 
     JOIN course_completions b ON a.user_id = b.user_id
 ),
-pair_courses AS (
+consecutive_pairs AS (
     SELECT first_course,
            second_course
-    FROM sequence_courses
-    WHERE second_course IS NOT NULL
+    FROM course_sequence
 )
 SELECT first_course,
        second_course,
        COUNT(*) AS transition_count
-FROM pair_courses
+FROM consecutive_pairs
 GROUP BY first_course, second_course
 ORDER BY transition_count DESC, first_course ASC, second_course ASC
