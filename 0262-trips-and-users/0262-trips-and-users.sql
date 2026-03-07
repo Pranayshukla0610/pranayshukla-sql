6deb4f8e7f4b91e8d97dd89dc564411d8a18f261
Pranay Shukla <pranayshukla1020@gmail.com>
2026-03-07 12:05:23 +0530

    Time: 552 ms (46.41%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,21 +1,22 @@
 # Write your MySQL query statement below
-WITH date_range AS (
-    SELECT 
+WITH data_sort AS (
+    SELECT t.client_id,
            t.status,
            t.request_at AS Day
     FROM Trips t 
-    JOIN Users c ON c.users_id = t.client_id AND c.banned = 'No'
-    JOIN Users d ON d.users_id = t.driver_id AND d.banned = 'No'
-    WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'  
+    JOIN Users a ON a.users_id = t.client_id 
+    JOIN Users b ON b.users_id = t.driver_id 
+    WHERE t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND a.banned = 'No' AND b.banned = 'No'
 ),
-calc_rate AS (
+rate AS (
     SELECT Day,
-           SUM(CASE WHEN status IN ('cancelled_by_driver','cancelled_by_client') 
-           THEN 1 ELSE 0 END) AS cancelled_trips,
-           COUNT(*) AS total_trips
-    FROM date_range
+           SUM(CASE WHEN status IN ('cancelled_by_driver','cancelled_by_client') THEN 
+           1 ELSE 0 END) AS cancelled_trips,
+           COUNT(*) AS Total_Users
+    FROM data_sort
     GROUP BY Day
 )
-SELECT Day, 
-      ROUND(cancelled_trips/total_trips, 2) AS `Cancellation Rate`
-FROM calc_rate
+SELECT Day,
+       ROUND(cancelled_trips/Total_Users,2) AS "Cancellation Rate"
+FROM rate
