4331f9464882965875cdf4d6990c23b7674f53d9
Pranay Shukla <pranayshukla1020@gmail.com>
2026-03-22 12:12:11 +0530

    Time: 275 ms (93.82%), Space: 0B (100%) - LeetHub

--- 3705-find-golden-hour-customers/3705-find-golden-hour-customers.sql
+++ 3705-find-golden-hour-customers/3705-find-golden-hour-customers.sql
@@ -1,35 +1,24 @@
-# Write your MySQL query statement below
-SELECT
-    customer_id,
-    COUNT(*) AS total_orders,
-    ROUND(
-        SUM(
-            CASE
-                WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
-                  OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
-                THEN 1 ELSE 0
-            END
-        ) * 100.0 / COUNT(*),
-        0
-    ) AS peak_hour_percentage,
-    ROUND(AVG(order_rating), 2) AS average_rating
-FROM restaurant_orders
-GROUP BY customer_id
-HAVING
-    COUNT(*) >= 3
-    -- at least 60% peak hour orders
-    AND SUM(
-        CASE
-            WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
-              OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
-            THEN 1 ELSE 0
-        END
-    ) * 1.0 / COUNT(*) >= 0.6
-    -- at least 50% orders rated
-    AND COUNT(order_rating) * 1.0 / COUNT(*) >= 0.5
-    -- average rating at least 4.0
-    AND AVG(order_rating) >= 4.0
-ORDER BY
+WITH sort_data AS (
+    SELECT customer_id,
+           ROUND(SUM(CASE WHEN TIME(order_timestamp) BETWEEN '11:00:00' AND '14:00:00'
+           OR TIME(order_timestamp) BETWEEN '18:00:00' AND '21:00:00'
+           THEN 1 ELSE 0 END)*100.0/COUNT(*)) AS peak_hour_percentage,
+           ROUND(AVG(order_rating),2) AS average_rating,
+           COUNT(order_id) AS total_orders,
+           COUNT(order_rating) AS rated_orders
+    FROM restaurant_orders
+    GROUP BY customer_id
+)
+SELECT customer_id,
+       total_orders,
+       peak_hour_percentage,
+       average_rating
+FROM sort_data
+WHERE total_orders >= 3
+AND average_rating >= 4.0
+AND rated_orders*1.0 / total_orders >= 0.5
+AND peak_hour_percentage >= 60
+ORDER BY 
     average_rating DESC,
-    customer_id DESC;
-
+    customer_id DESC
+       
